ed802bc569fffb8c93b25d6a320db8f45e5fbdd4
Toni Parviainen <tparvi@gmail.com>
2011-08-04 13:33:27 +0300

    Added dependency injection example

---
+++ Examples/DependencyInjectionExample/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DependencyInjectionExample
+{
+    using StructureMap;
+    using StructureMap.Configuration.DSL;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // ** IMPORTANT **
+                // Best practices when using StructureMap is out of the scope of this example.
+                // StructureMap is only used to give an example how settings could be injected
+                // into the application.
+                ObjectFactory.Initialize(x => x.AddRegistry<StructureMapRegistry>());
+
+                var service = ObjectFactory.GetInstance<SomeService>();
+
+                service.Authenticate();
+
+            }
+            catch (Exception exp)
+            {
+                Console.Error.WriteLine(exp.Message);
+            }
+        }
+    }
+
+    public class SomeService
+    {
+        private readonly ISettings settings;
+
+        [DefaultConstructor]
+        public SomeService(ISettings settings)
+        {
+            this.settings = settings;
+        }
+
+        public void Authenticate()
+        {
+            Console.WriteLine(
+                "Ready to authenticate with username {0} and password {1}",
+                this.settings.UserName,
+                this.settings.Password);
+
+            // call some service and give username and password
+            // this.Authentication(this.settings.UserName, this.settings.Password);
+        }
+    }
+
+    public class StructureMapRegistry : Registry
+    {
+        public StructureMapRegistry()
+        {
+            this.For<ISettings>().Singleton().Use<Settings>();
+            this.ForConcreteType<SomeService>();            
+        }
+    }
+
+}
---
+++ Examples/DependencyInjectionExample/Settings.cs
@@ -0,0 +1,28 @@
+﻿namespace DependencyInjectionExample
+{
+    using ApplicationSettings;
+
+    /// <summary>
+    /// This is the interface for the settings we like to expose.
+    /// We only expose settings we need using easy to use properties.
+    /// This interface can be mocked in unit tests.    
+    /// </summary>
+    public interface ISettings
+    {
+        string UserName { get; }
+        string Password { get; }
+    }
+
+    public class Settings : AppSettings, ISettings
+    {
+        public Settings()
+        {
+            this.UserName = this.GetValue("Username");
+            this.Password = this.GetValue("Password");
+        }
+
+        public string UserName { get; private set; }
+        
+        public string Password { get; private set; }
+    }
+}
