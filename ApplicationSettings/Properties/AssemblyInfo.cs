dda92a9b393e45df5d3b2cc77a5c305659873c56
Toni Parviainen <tparvi@gmail.com>
2011-08-04 11:05:32 +0300

    Added CLSCompliant(true)

--- ApplicationSettings/Properties/AssemblyInfo.cs
+++ ApplicationSettings/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -36,3 +37,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
 [assembly: InternalsVisibleTo("ApplicationSettingsTests")]
+[assembly: CLSCompliant(true)]
