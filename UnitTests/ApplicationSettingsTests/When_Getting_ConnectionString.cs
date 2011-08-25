4f71e119e11cf75b90df835d5305ef120e1389e6
Toni Parviainen <tparvi@gmail.com>
2011-08-25 20:01:30 +0300

    Removed unnecessary namespaces

--- UnitTests/ApplicationSettingsTests/When_Getting_ConnectionString.cs
+++ UnitTests/ApplicationSettingsTests/When_Getting_ConnectionString.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace ApplicationSettingsTests
+﻿namespace ApplicationSettingsTests
 {
-    using System.Reflection;
-
     using ApplicationSettings;
 
     using ApplicationSettingsTests.Configurations;
