68e91a78b0999566c9ede0a57791f3c34f47a53a
Toni Parviainen <tparvi@gmail.com>
2011-08-28 18:54:08 +0300

    Unused namespaces removed

--- ApplicationSettings/IgnoreProperty.cs
+++ ApplicationSettings/IgnoreProperty.cs
@@ -1,9 +1,6 @@
 ﻿namespace ApplicationSettings
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
 
     /// <summary>
     /// Describes that property should be ignored.
